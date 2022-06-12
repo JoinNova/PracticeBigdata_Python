1d4cc4f91be42cc7aaaec60067ed3590c678c918
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2022-06-12 09:16:34 +0900

    Add files via upload

---
+++ 0180.py
@@ -0,0 +1,6 @@
+'''
+데이터 출처 : https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset (후처리 작업)
+데이터 설명 : 뇌졸증 발생여부 예측
+train : https://raw.githubusercontent.com/Datamanim/datarepo/main/stroke_/train.csv
+test : https://raw.githubusercontent.com/Datamanim/datarepo/main/stroke_/test.csv
+'''
